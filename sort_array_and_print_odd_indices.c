d5e30a0a31e8eb81d28b261d7308b18a8078cfa7
KAUSHIK KUMAR REDDY SATHI <kaushikkumarreddys@gmail.com>
2026-02-06 21:56:40 +0530

    Implement array sorting and print odd indices
    The program reads an array of integers, sorts it using bubble sort, and prints the sorted array along with the elements at odd indices.

---
+++ sort_array_and_print_odd_indices.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                
+            }
+            
+            
+        }
+    }
+    
+    
+    printf("\n");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+      
+      
+      printf("\n");  
+    for(int i=0;i<size;i++){
+        if(i%2!=0){
+        printf("%d ",arr[i]);
+    }
+    }
+      return 0;  
+    }
